14d60f1a173f04b6ede598c3ce20d71cb0861f6a
unknown <victor.rdz.63@gmail.com>
2018-03-04 18:54:06 -0600

    Add overlay for each day of the calendar

---
+++ js/calendar.js
@@ -0,0 +1,11 @@
+function overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function overlayOff() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function test() {
+    document.getElementById("overlay").style.display
+}
