455c1d95e6f207644ac63ff3396c1fece5cd0fa2
unknown <victor.rdz.63@gmail.com>
2018-04-11 21:45:27 -0500

    Add week limit functionality

--- js/calendar.js
+++ js/calendar.js
@@ -1,37 +1,57 @@
-var year;
-var month;
-var week;
+var calDate; // Fecha (AÑO, MES);
 
-/*  Muestra el mes y el año en el encabezado.
-    Ej.-> "Marzo 2018"  , "Junio 2020". */
-function displayHeader(year, month) {
+$(document).ready(function() {
+
+    $("#calNext").click(function() {
+        calDate.setMonth(calDate.getMonth() + 1);
+        updateCalendar();
+    });
+
+    $("#calPrev").click(function() {
+        calDate.setMonth(calDate.getMonth() - 1);
+        updateCalendar();
+    });
+
+});
+
+
+/*  Muestra el mes y el año en el encabezado. */
+function displayHeader() {
     var mes = ["Enero", "Febrero", "Marzo", "Abril",
-    "Mayo", "Junio", "Julio", "Agosto",
-    "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-
-    $("#headerDate").text(mes[month] + " " + year);
+            "Mayo", "Junio", "Julio", "Agosto",
+            "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    $("#headerDate").text(mes[calDate.getMonth()] + " " + calDate.getFullYear());
 }  
 
-/*  Muestra los dias en el calendario.
-    firstDayWeek:   0 = Domingo ... 6 = Sabado.
-    lastDay:        28 ... 31 */
-function displayContent(firstDayWeek, lastDay) {
-    var day = 1;
-    var j = firstDayWeek;
+/*  Muestra los dias en el calendario. */
+function displayContent() {
+    var mes = ["Enero", "Febrero", "Marzo", "Abril",
+            "Mayo", "Junio", "Julio", "Agosto",
+            "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    var j = calDate.getUTCDay("1"); // First day of the week
+    var thisMonth = calDate.getMonth();
     $("#calendar .dayBox .hiddenRow").hide();
     for(var i = 0; i <= 5; i++) {
         for(j; j <= 6; j++) {
-            var id = "#" + i + "-" + j;
-            $(id).text(day);
-            $(id).css({
-                // modificar a las 2 semanas disponibles
-                "backgroundColor" : "rgb(255, 255, 255)",
-                "cursor" : "pointer"
-            });
-            day++;
-            if(day > lastDay) break;
+            var id = "#c" + i + "-" + j;
+            $(id).text(calDate.getDate());
+            $(id).removeClass("disponible");
+            $(id).addClass("normal");
+            // Si es un día valido para reservación... (Posibilidad de optimizar)
+            diasDisponibles.forEach(function(item) {
+                if(($("#headerDate").text().match("[A-Z][a-z]+") == mes[item.getMonth()]) && 
+                                ($(id).text() == item.getDate())) {
+                    $(id).addClass("disponible");
+                }
+            })
+            calDate.setDate(calDate.getDate() + 1);
+            if(calDate.getMonth() != thisMonth) break;
         }
-        if(day > lastDay) break;
+        if(calDate.getMonth() != thisMonth) {
+            calDate.setMonth(calDate.getMonth() - 1);
+            break;
+        }
         j = 0;
     }
 
@@ -41,25 +61,20 @@ function displayContent(firstDayWeek, lastDay) {
 }
 
 // Actualiza el calendario.
-function updateCalendar(year, month) {
+function updateCalendar() {
+    // Clear calendar.
     $("#calendar").animate( { "opacity" : .5 }, 80, function() {
         for(var i = 0; i <= 5; i++) {
             for(var j = 0; j <= 6; j++) {
-                var id = "#" + i + "-" + j;
-                $(id).text("").css({
-                    "backgroundColor" : "rgb(236, 236, 236)",
-                    "cursor" : "default"
-                });
+                var id = "#c" + i + "-" + j;
+                $(id).removeClass("normal");
+                $(id).text("").css("backgroundColor","rgb(236, 236, 236)");
             }
             j = 0;
         }
-        var date = new Date(year, month);
-        var lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-        firstDayWeek = date.getUTCDay("1");
-        lastDay = lastDate.getDate();
-
-        displayHeader(year, month);
-        displayContent(firstDayWeek, lastDay);
+        displayHeader();
+        displayContent();
+        
         $("#calendar").animate( { "opacity" : 1 }, 200);
     });
 }
--- php/date.php
+++ php/date.php
@@ -3,9 +3,9 @@
 
 $year = date("Y");
 $month = date("n");
-$week = date("W");
+$day = date("d");
 
-$fecha = array("var1" => $year, "var2" => $month, "var3" => $week);
+$fecha = array("var1" => $year, "var2" => $month, "var3" => $day);
 echo json_encode($fecha);
 
 ?>
